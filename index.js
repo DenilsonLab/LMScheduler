05e6ac478d0bc22a7e02fd4612a1a25f0d992455
Dlab <denilsonn1998@gmail.com>
2024-08-08 07:52:20 -0400

    Change in indexjs hello world

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3030;
+const port = 3000;
 
-// your code
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 
-app.listen(PORT, () => {
-  console.log(`server started on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
 });
